313ed7e5fe8f4c068205d56206a5add51f7f445b
halida <linjunhalida@gmail.com>
2014-02-18 21:00:34 +0800

    Declare field aliases

--- lib/my_mongoid/fields.rb
+++ lib/my_mongoid/fields.rb
@@ -16,35 +16,40 @@ module MyMongoid
     end
 
     module ClassMethods
-      def field(name)
-        add_field(name)
+      def field(*args)
+        add_field(*args)
       end
 
-      def add_field(name)
-        name = name.to_s
+      def add_field(*args)
+        name = args[0].to_s
+        opts = args[-1].kind_of?(Hash) ? args[-1] : {}
+
         raise DuplicateFieldError.new(name) if fields[name]
 
-        f = Field.new(name)
+        f = Field.new(name, opts)
 
         self.fields[name] = f
 
         define_method(name) do
           read_attribute(name)
         end
+        alias_method opts[:as], name if opts[:as]
 
         define_method("#{name}=") do |v|
           write_attribute(name, v)
         end
+        alias_method "#{opts[:as]}=", "#{name}=" if opts[:as]
       end
     end
 
   end
 
   class Field
-    attr_accessor :name
+    attr_accessor :name, :options
 
-    def initialize(name)
+    def initialize(name, options={})
       @name = name
+      @options = options
     end
   end
 end
